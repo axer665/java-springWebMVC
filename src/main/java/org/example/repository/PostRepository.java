47d05a4575ded1c748e1912f2eff25f65fc48cc3
axer665 <vellsevul@mail.ru>
2023-06-21 04:41:43 +0300

    Add soft deleted

--- src/main/java/org/example/repository/PostRepository.java
+++ src/main/java/org/example/repository/PostRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class PostRepository implements IPostRepository {
@@ -15,11 +16,16 @@ public class PostRepository implements IPostRepository {
     private List<Post> posts = new CopyOnWriteArrayList();
 
     public List<Post> all() {
-        return this.posts;
+        return posts.stream()
+                .filter(post -> !post.deleted())
+                .collect(Collectors.toList());
     }
 
     public Optional<Post> getById(long id) {
-        return posts.stream().filter(post -> post.getId() == id).findFirst();
+        return posts.stream()
+                .filter(post -> post.getId() == id)
+                .filter(post -> !post.deleted())
+                .findFirst();
     }
 
     public Post save(Post post) {
@@ -28,12 +34,11 @@ public class PostRepository implements IPostRepository {
             postId.addAndGet(1);
         } else {
             for (Post searchPost: this.posts) {
-                if (post.getId() == searchPost.getId()){
+                if (post.getId() == searchPost.getId() && !searchPost.deleted()){
                     searchPost.setContent(post.getContent());
                     return searchPost;
                 }
             }
-            // если не нашли - кидаем исключение
             throw new NotFoundException("Post not found");
         }
         this.posts.add(post);
@@ -44,7 +49,7 @@ public class PostRepository implements IPostRepository {
         for (int i = 0; i < this.posts.size(); i++) {
             Post searchPost = this.posts.get(i);
             if (searchPost.getId() == id) {
-                this.posts.remove(i);
+                searchPost.delete();
             }
         }
     }
